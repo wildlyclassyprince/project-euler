f6b78bea8f1d5603c574a0922666167297b37933
wildlyclassyprince <lihtumb@gmail.com>
2020-06-23 01:33:07 +0400

    Update sieve

--- largest-prime-factor/challenge_test.go
+++ largest-prime-factor/challenge_test.go
@@ -5,16 +5,18 @@ import (
 	"testing"
 )
 
+func TestPrimes(t *testing.T) {
+	t.Run("test prime numbers", func(t *testing.T) {
+		got := sieveOfAtkin(3)
+		want := []int{2}
+		assertSequence(t, got, want)
+	})
+}
+
 func TestPrimeFactors(t *testing.T) {
 	t.Run("test all factors of 13195", func(t *testing.T) {
 		got := generateFactors(13195)
 		want := []int{1, 5, 7, 13, 29, 35, 65, 91, 145, 203, 377, 455, 1015, 1885, 2639}
-		assertSequence(t, got, want)
-	})
-	t.Run("test prime-ness of factors of 13195", func(t *testing.T) {
-		seq := generateFactors(13195)
-		got := atkinSieve(seq)
-		want := []int{5, 7, 13, 29}
 		assertSequence(t, got, want)
 	})
 }
